71b8db9e3391e876149b865d4bd1cdd6fee775f9
Rafael Maltez <rafaelmaltez89@gmail.com>
2021-01-26 17:08:52 -0300

    Exercise2: add a list inside BroswerRout with three Links in it

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import Home from './Home';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Link } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
+      <span> <Link to="/">Home</Link></span>
+      <span> <Link to="/about">About</Link></span>
+      <span> <Link to="/users">Users</Link></span>
         <Home />
       </BrowserRouter>
     );
